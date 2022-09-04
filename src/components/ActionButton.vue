91b60a809569953893edac34af4c5cc8be124fa4
ddevtk <buiducduong2342000@gmail.com>
2022-09-04 16:29:30 +0700

    create action button component

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="px-5 py-3 bg-brand-blue-1 text-white rounded font-medium hover:shadow-blue-custom"
+  >
+    Sign in
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
